e38fcc328895f1931906fefbb3311a079cfb7766
João Gabriel <joaogabrielrcardoso@gmail.com>
2022-09-22 16:49:51 -0300

    Getting User Input & Handling Form Submission

--- src/components/meetups/NewMeetupForm.jsx
+++ src/components/meetups/NewMeetupForm.jsx
@@ -1,38 +1,53 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import Card from '../ui/Card';
 import styles from './NewMeetupForm.module.css';
 
 function NewMeetupForm() {
+  const titleInputRef = useRef();
+  const imageInputRef = useRef();
+  const addressInputRef = useRef();
+  const descriptionInputRef = useRef();
+
+  const submitHandle = (event) => {
+    event.preventDefault();
+    const title = titleInputRef.current.value;
+    const image = imageInputRef.current.value;
+    const address = addressInputRef.current.value;
+    const description = descriptionInputRef.current.value;
+
+    const meetupData = { title, image, address, description };
+    console.log(meetupData);
+  };
+
   return (
     <Card>
-      <form action="" method="get" className={styles.form}>
+      <form className={styles.form} onSubmit={submitHandle}>
         <div className={styles.control}>
           <label htmlFor="title">Title</label>
           <input
             type="text"
-            name="title"
             id="title"
+            ref={titleInputRef}
             required
-            autoComplete="off"
             autoFocus
           />
         </div>
 
         <div className={styles.control}>
           <label htmlFor="image">Image</label>
-          <input type="url" name="image" id="image" required />
+          <input type="url" id="image" ref={imageInputRef} required />
         </div>
 
         <div className={styles.control}>
           <label htmlFor="address">Address</label>
-          <input type="text" name="address" id="address" required />
+          <input type="text" id="address" ref={addressInputRef} required />
         </div>
 
         <div className={styles.control}>
           <label htmlFor="description">Description</label>
           <textarea
-            name="description"
             id="description"
+            ref={descriptionInputRef}
             cols="30"
             rows="5"
             required
