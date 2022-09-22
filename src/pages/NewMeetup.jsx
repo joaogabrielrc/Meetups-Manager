9bd8fda0eb6a1e62765f0c3afe7557a91dbf6d95
João Gabriel <joaogabrielrcardoso@gmail.com>
2022-09-22 17:29:40 -0300

    Navigating Programmatically

--- src/pages/NewMeetup.jsx
+++ src/pages/NewMeetup.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import NewMeetupForm from '../components/meetups/NewMeetupForm';
 
 function NewMeetup() {
-  const addMeetupHandler = (meetupData) => {
+  const history = useHistory();
+
+  const addMeetupHandler = async (meetupData) => {
     const requestOptions = {
       method: 'POST',
       body: JSON.stringify(meetupData),
@@ -11,10 +14,12 @@ function NewMeetup() {
       },
     };
 
-    fetch(
+    await fetch(
       'https://react-getting-started-d3117-default-rtdb.firebaseio.com/meetups.json',
       requestOptions
     );
+
+    history.replace('/');
   };
 
   return (
