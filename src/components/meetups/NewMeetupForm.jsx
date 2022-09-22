5df203a8814decf876d0657411889ca8037ca7c2
João Gabriel <joaogabrielrcardoso@gmail.com>
2022-09-22 17:11:13 -0300

    Preparing The App For Http Requests & Adding a Backend

--- src/components/meetups/NewMeetupForm.jsx
+++ src/components/meetups/NewMeetupForm.jsx
@@ -2,7 +2,7 @@ import React, { useRef } from 'react';
 import Card from '../ui/Card';
 import styles from './NewMeetupForm.module.css';
 
-function NewMeetupForm() {
+function NewMeetupForm(props) {
   const titleInputRef = useRef();
   const imageInputRef = useRef();
   const addressInputRef = useRef();
@@ -16,7 +16,8 @@ function NewMeetupForm() {
     const description = descriptionInputRef.current.value;
 
     const meetupData = { title, image, address, description };
-    console.log(meetupData);
+
+    props.onAddMeetup(meetupData);
   };
 
   return (
--- src/pages/NewMeetup.jsx
+++ src/pages/NewMeetup.jsx
@@ -2,10 +2,14 @@ import React from 'react';
 import NewMeetupForm from '../components/meetups/NewMeetupForm';
 
 function NewMeetup() {
+  const addMeetupHandler = (meetupData) => {
+    console.log(meetupData);
+  };
+
   return (
     <section>
       <h1>Add New Meetup</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </section>
   );
 }
